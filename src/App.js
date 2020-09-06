a0cb938f22b9b15ec966718844612dc218f3db16
atebin <a.tebin@gmail.com>
2020-09-06 19:08:04 +0300

    SF_kanban - master: first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="">
+        <p>
+          Тест !!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
