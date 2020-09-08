578a86c149c12e83beee9cf19f4a44a2e7b3f276
atebin <a.tebin@gmail.com>
2020-09-08 22:27:35 +0300

    SF_kanban - master: create test data for application

---
+++ src/Components/App/App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+
+  	constructor() {
+    	super();
+
+    	this.state = {
+      		tasks: {},
+      		test: '',
+    	}
+  	}
+
+  	componentDidMount() {
+
+		let user = {};
+		
+		// Проверяем localStorage
+	
+		let jsonStorage = localStorage.getItem('appKanban_atebin');
+		//jsonStorage = null; // раскомментировать для записи в localStorage тестового значения
+
+		if (jsonStorage !== null) {
+			// Если в localStorage есть данные для приложения appKanban, считываем их
+			console.log('data from localStorage');
+
+			user = JSON.parse(jsonStorage);
+			console.log(user);
+		} else {
+			// Если в localStorage ничего нет, создаем тестовый массив
+
+			console.log('create test data!!!');
+
+			let tasks = {};
+
+			for (let i=1; i<=5; i++) {
+				let task = {};
+				
+				task['task'] = 'task #' + i;
+				task['active'] = true;
+				task['status'] = 1;
+				task['date1'] = '2020/09/06';
+				task['date2'] = '';
+				task['date3'] = '';
+				task['date4'] = '';
+				task['detail'] = 'lorem  ipsum ...#' + i;
+
+				tasks['task ' + i] = task;
+			}
+			user['atebin'] = tasks;
+		}
+
+		// сохраняем текущее состояние
+		this.setState({ tasks: user });
+		localStorage.setItem('appKanban_atebin', JSON.stringify(user));
+  	}
+
+
+  render() {
+
+    return (
+      <div>
+        <header className="">
+          <p>
+            Тестовая строка !!!
+          </p>
+          <p>{ /*qwe.get('atebin')*/ }</p>
+        </header>
+      </div>
+    );  
+  }
+
+}
+
+export default App;
